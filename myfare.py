71237c7332d78f7161cfe0000f36cc2d79cf4365
john <jacgarrett18@gmail.com>
2019-08-19 19:30:05 -0400

    python3 support

--- myfare.py
+++ myfare.py
@@ -5,7 +5,7 @@ prot = {
     'enquiry_module': '\x03\x12\x00\x15',
     'enquiry_module_return': '\x02\x12\x14',
     'active_buzzer': '\x02\x13\x15',
-    'enquiry_card': pack('BBBB', 03, 02, 00, 05),
+    'enquiry_card': pack('BBBB', 0o3, 0o2, 00, 0o5),
     'enquiry_cards_return': '\x03\x02\x01\x06', # got valid card
     'enquiry_no_card_found': '\x02\x01\x03', # no card reachable or invalid
     'enquiry_all_cards': '\x03\x02\x01\x05',
@@ -18,16 +18,18 @@ ser.write(prot['enquiry_card'])
 resp =  ser.read(4)
 
 if resp == prot['enquiry_no_card_found']:
-    print "no valid card reachable"
+    print("no valid card reachable")
 elif resp == prot['enquiry_cards_return']:
     ser.write(prot['active_buzzer'])
     resp = ser.read(3)
     if resp == prot['active_buzzer']:
-        print "found card, sending anticollision"
+        print("found card, sending anticollision")
         ser.write(prot['anticollision'])
         resp = ser.read(7)
         header = resp.encode('hex')[:4]
         if header == "0603":
-            print "Card Serial:" + resp[-5:].encode('hex')
+            print("Card Serial:" + resp[-5:].encode('hex'))
+else:
+    print('an error occured')
 
 ser.close()
